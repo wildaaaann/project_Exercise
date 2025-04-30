10f021875d9875e78d11fd968d3fcdc1fca2f778
wildan zalul <m.wildanzalul@gmail.com>
2025-05-01 01:17:27 +0700

    Add files via upload

---
+++ M.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a; 
+    int b[3 * a];
+    scanf("%d", &a);
+
+    for (int i = 0; i < 3 * a; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    for (int i = 0; i < 3 * a - 1; i++) {
+        for (int j = 0; j < 3 * a - i - 1; j++) {
+            if (b[j] > b[j + 1]) {
+                int temp = b[j];
+                b[j] = b[j + 1];
+                b[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("%d\n", b[a]);
+    return 0;
+}
